bb45c21aa77d6d4081167fb3cb35a1e52b2d7d05
surin.i <surin.i@skbkontur.ru>
2025-02-09 16:04:23 +0500

    fix problems with build assimp

--- cmake/LinkLibraries.cmake
+++ cmake/LinkLibraries.cmake
@@ -1,16 +1,25 @@
-add_subdirectory(libraries/glfw)
-target_link_libraries(${PROJECT_NAME} glfw)
+set(SAVED_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+set(SAVED_CMAKE_C_FLAGS ${CMAKE_C_FLAGS})
 
-add_subdirectory(libraries/glad)
-target_link_libraries(${PROJECT_NAME} glad)
-
-add_subdirectory(libraries/glm)
-target_link_libraries(${PROJECT_NAME} glm)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -w")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -w")
 
 add_subdirectory(libraries/assimp)
-target_link_libraries(${PROJECT_NAME} assimp)
+target_link_libraries(${PROJECT_NAME} PRIVATE assimp)
+
+add_subdirectory(libraries/glfw)
+target_link_libraries(${PROJECT_NAME} PRIVATE glfw)
+
+#include_directories(libraries/glad/)
+#target_link_libraries(${PROJECT_NAME} PRIVATE glad)
+
+add_subdirectory(libraries/glm)
+target_link_libraries(${PROJECT_NAME} PRIVATE glm)
 
 add_subdirectory(libraries/spdlog)
-target_link_libraries(${PROJECT_NAME} spdlog::spdlog)
+target_link_libraries(${PROJECT_NAME} PRIVATE spdlog::spdlog)
 
 include_directories(libraries/stb_image)
+
+set(CMAKE_CXX_FLAGS ${SAVED_CMAKE_CXX_FLAGS})
+set(CMAKE_C_FLAGS ${SAVED_CMAKE_C_FLAGS})
